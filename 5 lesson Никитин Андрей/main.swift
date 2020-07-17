2b70cd22c7d1670657fa22ef5969673dbce16d09
Rayen <zufirka@icloud.com>
2020-07-17 06:28:56 +0300

    Initial Commit

---
+++ 5 lesson Никитин Андрей/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  5 lesson Никитин Андрей
+//
+//  Created by Rayen on 7/17/20.
+//  Copyright © 2020 Rayen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
